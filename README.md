89391c36df6a4f5a49795619a697689bb565402b
Tomás Ciccola <tciccola@digital-democracy.com>
2025-05-08 11:26:13 -0300

    first

---
+++ README.md
@@ -0,0 +1,17 @@
+# simpler github trigger
+This proxy allows triggering a github workflow from enviroments where one can set up a webhook but without the ability to set a request body (f.e. a notion button), since github expects as a minimum a body of `{event_type: job_name}`.
+
+Its made as a fly.io service so to deploy it one can just clone the repo and run
+```bash
+flyctl deploy --app <your app name>
+```
+
+Additionally there's a dockefile
+
+it expects the following secrets to be set on the environment
+```bash
+EVENT_TYPE=job_name
+GITHUB_TOKEN=# with at least repo:read permission
+REPO=# user/repo where the workflow lives
+SECRET=# extra measure to disallow random requests
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import axios from 'axios'
+
+const app = express();
+app.use(express.json());
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const REPO = process.env.REPO;
+const SECRET = process.env.SECRET
+const EVENT_TYPE = process.env.EVENT_TYPE
+
+app.post('/trigger', auth, async (req,res) => {
+  try {
+    const payload = {
+      "event_type": EVENT_TYPE
+    }
+    const URL = `https://api.github.com/repos/${REPO}/dispatches`
+    const headers = {
+      'Authorization': `token ${GITHUB_TOKEN}`,
+      'Accept': 'application/vnd.github+json'
+    }
+    console.log('sending', URL, payload, headers)
+    await axios.post(URL, payload, {headers: headers})
+    res.status(200).send("GitHub workflow triggered.");
+  }catch(err){
+    console.error(err.response?.data || err.message);
+    res.status(500).send("Failed to trigger GitHub.");
+  }
+})
+
+function auth (req,res,next) {
+  const token = req.headers['x-api-key']
+  console.log("AUTH", token)
+  if (token !== SECRET) return res.status(403).json({error: 'forbidden'})
+  next()
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
