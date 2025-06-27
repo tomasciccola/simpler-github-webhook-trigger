a3ea9c9559a819510b5ea25c2c4f2d856987dda8
Tomás Ciccola <tciccola@digital-democracy.com>
2025-06-27 13:03:39 -0300

    move event to a header, add better logging

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const REPO = process.env.REPO;
 const BRANCH = process.env.BRANCH
 const SECRET = process.env.SECRET
-const EVENT_TYPE = process.env.EVENT_TYPE
 
 app.post('/trigger', auth, async (req,res) => {
   try {
+    const event = req.headers['event']
     const payload = {
       "ref": BRANCH
     }
-    const URL = `https://api.github.com/repos/${REPO}/actions/workflows/${EVENT_TYPE}/dispatches`
+    const URL = `https://api.github.com/repos/${REPO}/actions/workflows/${event}/dispatches`
     const headers = {
       'Authorization': `token ${GITHUB_TOKEN}`,
       'Accept': 'application/vnd.github+json'
     }
     console.log(`triggering ${EVENT_TYPE} on repo: ${REPO}/${BRANCH}`)
+    console.log(`url : ${URL}`)
     await axios.post(URL, payload, {headers: headers})
     res.status(200).send("GitHub workflow triggered.");
   }catch(err){
     console.error(err.response?.data || err.message);
-    res.status(500).send("Failed to trigger GitHub.");
+    res.status(500).send(`Failed to trigger GitHub.
+    ${JSON.stringify(err.response?.data || err.message)}`);
   }
 })
 
