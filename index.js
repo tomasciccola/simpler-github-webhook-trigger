16a9d745559244134cd85b6de871c5e34b710e1c
Tomás Ciccola <tciccola@digital-democracy.com>
2026-02-18 14:48:02 -0300

    change proxy to point to our own CI. additional secret check

--- index.js
+++ index.js
@@ -4,36 +4,40 @@ import axios from 'axios'
 const app = express();
 app.use(express.json());
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const REPO = process.env.REPO;
-const BRANCH = process.env.BRANCH
+const TOKEN = process.env.TOKEN;
 const SECRET = process.env.SECRET
 
 app.post('/trigger', auth, async (req,res) => {
   try {
     const event = req.headers['event']
+    const options = JSON.parse(req.headers['options'])
     const payload = {
-      "ref": BRANCH
+      "type": event,
+      "options": options
     }
-    const URL = `https://api.github.com/repos/${REPO}/actions/workflows/${event}/dispatches`
+    const URL = `http://137.184.153.36:3001/jobs`
     const headers = {
-      'Authorization': `token ${GITHUB_TOKEN}`,
-      'Accept': 'application/vnd.github+json'
+      'Authorization': `Bearer ${TOKEN}`,
+      'Content-Type': 'application/json',
     }
-    console.log(`triggering ${EVENT_TYPE} on repo: ${REPO}/${BRANCH}`)
+    console.log(`triggering ${event}`)
+    console.log(`with payload ${JSON.stringify(payload, null, 4)}`)
     console.log(`url : ${URL}`)
-    await axios.post(URL, payload, {headers: headers})
+    const response = await axios.post(URL, payload, {headers: headers})
+    console.log(`got response from CI: ${JSON.stringify(response.data, null, 4)}`)
     res.status(200).send("GitHub workflow triggered.");
   }catch(err){
     console.error(err.response?.data || err.message);
-    res.status(500).send(`Failed to trigger GitHub.
+    res.status(500).send(`Failed to trigger builder.
     ${JSON.stringify(err.response?.data || err.message)}`);
   }
 })
 
 function auth (req,res,next) {
   const token = req.headers['x-api-key']
-  if (token !== SECRET) return res.status(403).json({error: 'forbidden'})
+  if (token !== SECRET || token === undefined || token === null){
+    return res.status(403).json({error: 'forbidden'})
+  }
   next()
 }
 
